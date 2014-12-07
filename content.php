ed062104924bcb026fe878c8b21267bebbb8f0e6
jeonghwan <ej88ej@gmail.com>
2014-12-07 19:33:09 +0900

    single.php 디자인 수정

--- content.php
+++ content.php
@@ -22,7 +22,7 @@
 			endif; ?>
 
 			<?php if ( is_single() ) : ?>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<h1 class="entry-title entry-title-underline"><?php the_title(); ?></h1>
 			<?php else : ?>
 			<h1 class="entry-title">
 				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
